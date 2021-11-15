a49f742d9d8c644895e04753fadda2d3a67d1b54
Maxim <mberdenev@mail.ru>
2021-11-15 10:30:29 +0300

    made first main page

--- src/components/FirstPage/FirstPage.jsx
+++ src/components/FirstPage/FirstPage.jsx
@@ -6,8 +6,16 @@ import arrowDown from '../../assets/img/arrowDown.svg';
 import treatment from '../../assets/img/treatment.png';
 import treatment2 from '../../assets/img/treatment2.png';
 import treatment3 from '../../assets/img/treatment3.png';
+import star from '../../assets/img/star.svg';
+import amazonPrime from '../../assets/img/amazonPrime.svg';
 
 const FirstPage = () => {
+
+    const stars = [];
+    for(let i=0;i<5;i++){
+        stars.push(<img src={star} alt={'star'}/>)
+    }
+
     return (
         <div>
             <div className={s.Preview}>
@@ -15,7 +23,8 @@ const FirstPage = () => {
                     <div className={s.carousel}>
                         <img src={arrowUp} alt=""/>
                         <div className={s.images}>
-                            <div className={`${s.imageWrapper} ${s.activeImageWrapper}`}><img src={treatment} alt=""/></div>
+                            <div className={`${s.imageWrapper} ${s.activeImageWrapper}`}><img src={treatment} alt=""/>
+                            </div>
                             <div className={s.imageWrapper}><img src={treatment2} alt=""/></div>
                             <div className={s.imageWrapper}><img src={treatment3} alt=""/></div>
                         </div>
@@ -23,8 +32,20 @@ const FirstPage = () => {
                     </div>
                     <img className={s.image} src={bigTreatment} alt="bigTreatment"/>
                 </div>
-                <div>
-
+                <div className={s.infoBlock}>
+                    <div className={s.starsBlock}>
+                        <div>BEST SELLER</div>
+                        <div>{stars} <span>(1731 reviews)</span></div>
+                    </div>
+                    <div className={s.title}>Acne Treatment Cystic Acne heals, repairs & renews</div>
+                    <div className={s.descriptionTitle}>Description:</div>
+                    <div className={s.description}>Addictively refreshing gel-cream leaves skin
+                        plump, dewy, glowing.</div>
+                    <div className={s.itemsLeft}>items left: 12</div>
+                    <div className={s.line}></div>
+                    <div className={s.price}><span>us$25.89</span> $0.00 FREE</div>
+                    <button className={s.buttonSelect}>Select</button>
+                    <div className={s.logoAmazonPrime}><img src={amazonPrime} alt="amazonPrime"/></div>
                 </div>
             </div>
             <div>
